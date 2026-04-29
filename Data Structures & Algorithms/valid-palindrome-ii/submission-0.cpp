28c5c91ad031c76088db758090fad419e18c1ecf
Akram Sharief Shaik <82010523+Alexmercer116@users.noreply.github.com>
2026-04-29 21:01:59 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    bool isPalindrome(string s,int l,int r){
+        while(l<r){
+            if(s[l++]!=s[r--]) return false;
+        }
+        return true;
+    }
+public:
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+               return (isPalindrome(s,i+1,j)||isPalindrome(s,i,j-1));
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
