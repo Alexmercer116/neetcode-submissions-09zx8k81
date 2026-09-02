0454f2057efa9acba0cd5a8aad428e58129489a9
Akram Sharief Shaik <82010523+Alexmercer116@users.noreply.github.com>
2026-09-02 22:29:16 +0530

    Add: find-k-closest-elements - submission-5

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int pos = lower_bound(arr.begin(),arr.end(),x)-arr.begin();
+        int l = pos-1,r=pos;
+        vector<int> res;
+        while(l>=0&&r<arr.size()&&(res.size()<k)){
+            if(abs(arr[l]-x)<=(abs(arr[r]-x))) res.push_back(arr[l--]);
+            else res.push_back(arr[r++]);
+        }
+        while(l>=0&&(res.size()<k)) res.push_back(arr[l--]);
+        while(r<arr.size()&&(res.size()<k)) res.push_back(arr[r++]);
+        sort(res.begin(),res.end());
+
+        return res;
+    }
+};
